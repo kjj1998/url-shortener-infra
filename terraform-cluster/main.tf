b16d222a033a48bb9167adf80e099eb59c061cf7
Koh Jun Jie <koh.jun.jie@outlook.com>
2024-05-21 00:05:32 +0800

    changed auth cluster token retrieval

--- terraform-cluster/main.tf
+++ terraform-cluster/main.tf
@@ -36,12 +36,12 @@ data "aws_eks_cluster" "cluster" {
 provider "helm" {
   kubernetes {
     host = data.aws_eks_cluster.cluster.endpoint
-    # token                  = data.aws_eks_cluster_auth.cluster_auth.token 
-    exec {
-      api_version = "client.authentication.k8s.io/v1beta1"
-      args        = ["eks", "get-token", "--cluster-name", var.eks_cluster_name, "--output", "json"]
-      command     = "aws"
-    }
+    token                  = data.aws_eks_cluster_auth.cluster_auth.token
+    # exec {
+    #   api_version = "client.authentication.k8s.io/v1beta1"
+    #   args        = ["eks", "get-token", "--cluster-name", var.eks_cluster_name, "--output", "json"]
+    #   command     = "aws"
+    # }
     cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
   }
 }
