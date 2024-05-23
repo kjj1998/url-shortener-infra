fe33aceb92ec8aa13506131982354ad65889076d
Koh Jun Jie <koh.jun.jie@outlook.com>
2024-05-23 21:56:25 +0800

    remove formatting

--- load-balancer-controller/modules/aws-load-balancer-controller/main.tf
+++ load-balancer-controller/modules/aws-load-balancer-controller/main.tf
@@ -64,8 +64,8 @@ provider "kubernetes" {
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
   exec {
     api_version = "client.authentication.k8s.io/v1beta1"
-    args        = ["eks", "get-token", "--cluster-name", var.cluster_name, "--profile", "admin-1"]
-    # args        = ["eks", "get-token", "--cluster-name", var.cluster_name]
+    # args        = ["eks", "get-token", "--cluster-name", var.cluster_name, "--profile", "admin-1"]
+    args        = ["eks", "get-token", "--cluster-name", var.cluster_name]
     command = "aws"
   }
 }
