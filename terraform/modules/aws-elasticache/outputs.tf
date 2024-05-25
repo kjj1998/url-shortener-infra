fa175fe2967c0820ab1071829e7d6038b0a28f68
Koh Jun Jie <koh.jun.jie@outlook.com>
2024-05-25 18:35:21 +0800

    deleted terraform folder

--- terraform/modules/aws-elasticache/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "elasticache_id" {
-  description = "ID of the elasticache"
-  value       = aws_elasticache_replication_group.redis.id
-}
-
-output "elasticache_endpoint" {
-  description = "Endpoint of the elasticache"
-  value       = aws_elasticache_replication_group.redis.primary_endpoint_address
-}
--- terraform/variables.tf
+++
@@ -1,5 +0,0 @@
-variable "aws_region" {
-  description = "The AWS region to deploy resources"
-  type        = string
-  default     = "ap-southeast-1"
-}
